2fe9a422e35e6bf57c62ecdfa00e2cf34db894e4
rcraig <ryanpcraig1@gmail.com>
2021-10-07 12:20:04 -0500

    ready to test get method

--- client/src/components/PostCard.vue
+++ client/src/components/PostCard.vue
@@ -2,12 +2,19 @@
   <div>
     <h2>{post.username}</h2>
     <p>{post.post}</p>
-    <button></button>
+    <button @click="deletePost()"></button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PostCard'
+  name: 'PostCard',
+  props: ['post'],
+  methods: {
+    deletePost(postId) {
+      console.log("delete ran")
+      // await axios.delete(``)
+    }
+  }
 }
 </script>
