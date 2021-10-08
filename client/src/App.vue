c5452892f6b246ba6ebc1026141ade84c559853e
Reinel Parrado <reinelparrado@gmail.com>
2021-10-07 22:41:20 -0500

    Modal

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Modal />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Modal
   }
 }
 </script>
---
+++ client/src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <BButton v-b-modal.modal-1>Launch demo modal</BButton>
+
+      <BModal id="modal-1" title="BootstrapVue">
+        <p class="my-4">Hello from modal!</p>
+      </BModal>
+    </div>
+  </div>
+</template>
+
+<script>
+import {BModal, BButton} from 'bootstrap-vue'
+export default {
+  name: 'Modal',
+  components: {
+    BModal,
+    BButton
+  }
+
+}
+</script>
+
+<style>
+
+</style>
