d4ac75c0175dc63bb6e2746b20d950f24a57f5c7
Reinel Parrado <reinelparrado@gmail.com>
2021-10-08 11:31:09 -0500

    fixed post request

--- client/src/components/Modal.vue
+++ client/src/components/Modal.vue
@@ -6,70 +6,63 @@
       <b-modal
       id="modal-prevent-closing"
       ref="modal"
-      title="Submit Your Name"
-    >
-      <form ref="form" @submit.stop.prevent="onSubmit">
+      title="Submit Your Post"
+      hide-footer
+      >
+      <b-form ref="form" @submit="newPost">
         <b-form-group
           label="username"
           label-for="username-input"
           invalid-feedback="Username is required"
-          :state="username"
+          
         >
           <b-form-input
-            id="username-input"
+            id="username-input" 
+            
             v-model="username"
-            :state="username"
             required
           ></b-form-input>
         </b-form-group>
-        <b-form-group label="post" label-for="post-input" invalid-feedback="Content is required" :state="post">
-          <b-form-input id="conent-input" v-model="post" :state="post"></b-form-input>
+
+        <b-form-group label="post" label-for="post-input" invalid-feedback="Content is required" >
+          <b-form-input id="post-input" v-model="post" ></b-form-input>
         </b-form-group>
-      </form>
+
+      <b-button class="mt-3" block @click="newPost">Submit</b-button>
+      </b-form>
     </b-modal>
     </div>
   </div>
 </template>
 
 <script>
-import {BModal, BButton, BFormInput, BFormGroup} from 'bootstrap-vue'
+import {BModal, BButton, BFormInput, BForm, BFormGroup} from 'bootstrap-vue'
 import axios from 'axios'
 import {BASE_URL} from '../globals'
 export default {
   name: 'Modal',
   data: () =>({
-    username: null,
-    post: null
+    username: '',
+    post: ''
   }),
   components: {
     BModal,
     BButton,
     BFormInput,
+    BForm,
     BFormGroup
   },
   methods: {
       async newPost(){
-        await axios.post(`${BASE_URL}/post`, 
+        const res = await axios.post(`${BASE_URL}/post`, 
         {
           username: this.username,
           post: this.post
         })
-      },
-      onSubmit(event) {
-        event.preventDefault()
-        alert(JSON.stringify(this.form))
-        this.newPost()
-      },
-      onReset(event) {
-        event.preventDefault()
-        // Reset our form values
-        this.form.username = ''
-        this.form.post = ''
-        // Trick to reset/clear native browser form validation state
-        this.show = false
-        this.$nextTick(() => {
-          this.show = true
-        })
+        this.username = ''
+        this.post = ''
+        location.reload()
+        return res
       }
   }
 }
